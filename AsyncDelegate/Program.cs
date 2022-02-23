2a86ca6b2a1d1d21f4c427b23023dc2dfe1377f3
GreedNeSS <ruslan199126@gmail.com>
2022-02-23 17:52:50 +0300

    Added async delegate

---
+++ AsyncDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AsyncDelegate
+{
+    class Program
+    {
+        public delegate int BinaryOp(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Sync Delegate Review *****");
+            Console.WriteLine($"Main() invoked to thread: {Thread.CurrentThread.ManagedThreadId}");
+
+            BinaryOp binaryOp = new BinaryOp(Add);
+            IAsyncResult ar = binaryOp.BeginInvoke(10, 10, null, null);
+
+            //while (!ar.IsCompleted)
+            //{
+            //    Console.WriteLine("Doing more work in Main().");
+            //    Thread.Sleep(1000);
+            //}
+
+            while (!ar.AsyncWaitHandle.WaitOne(1000, true))
+            {
+                Console.WriteLine("Doing more work in Main().");
+            }
+
+            int answer = binaryOp.EndInvoke(ar);
+            
+            Console.WriteLine($"10 + 10 is {answer}");
+            Console.ReadLine();
+        }
+
+        static int Add(int a, int b)
+        {
+            Console.WriteLine($"Add() invoked to thread: {Thread.CurrentThread.ManagedThreadId}");
+
+            Thread.Sleep(5000);
+
+            return a + b;
+        }
+    }
+}
