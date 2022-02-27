c7d3bc8104a1a0bbc0de5c7880b0608982c01df8
GreedNeSS <ruslan199126@gmail.com>
2022-02-27 14:08:23 +0300

    Added parameterized thread start example

---
+++ ParameterizedThreadStartExample/AddParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterizedThreadStartExample
+{
+    class AddParams
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public AddParams(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+    }
+}
---
+++ ParameterizedThreadStartExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ParameterizedThreadStartExample
+{
+    class Program
+    {
+        private static AutoResetEvent waitHandler = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Using Parameterized Thread Start *****");
+            Console.WriteLine($"ID of thread in Main(): {Thread.CurrentThread.ManagedThreadId}");
+
+            AddParams addParams = new AddParams(27, 10);
+            Thread newThread = new Thread(new ParameterizedThreadStart(Add));
+
+            newThread.Start(addParams);
+
+            waitHandler.WaitOne();
+
+            Console.ReadLine();
+        }
+
+        static void Add(object addParams)
+        {
+            if (addParams is AddParams @params)
+            {
+                Console.WriteLine($"ID of thread in Add(): {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"{@params.A} + {@params.B} = {@params.A + @params.B}");
+            }
+
+            waitHandler.Set();
+        }
+    }
+}
