765894133afae30d7d0d4a775d3294fe2bbed92b
GreedNeSS <ruslan199126@gmail.com>
2022-02-27 15:48:16 +0300

    Used lock

---
+++ MultiThreadLocker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadLocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Synchronizing Threads *****\n");
+
+            Printer p = new Printer();
+
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers))
+                {
+                    Name = $"Worker thread #{i}"
+                };
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+        }
+    }
+}
