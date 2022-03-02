108d558fc42d34ea4ab8442f9298e3def4e88cbd
GreedNeSS <ruslan199126@gmail.com>
2022-03-02 16:42:17 +0300

    Added Timer example

---
+++ TimerApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TimerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimerCallback callback = new TimerCallback(PrintTime);
+            var _ = new Timer(callback, "Hello Ruslan", 0, 1000);
+
+            Console.ReadLine();
+        }
+
+        static void PrintTime(object state)
+        {
+            Console.WriteLine($"Time: {DateTime.Now.ToLongTimeString()} Message: {state.ToString()}");
+        }
+    }
+}
