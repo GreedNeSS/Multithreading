d24a870569f04c0f24c8e2e8a8843105a92078ae
GreedNeSS <ruslan199126@gmail.com>
2022-03-04 19:55:40 +0300

    Added semaphore example

---
+++ SemaphoreApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Reader reader = new Reader(i);
+            }
+        }
+    }
+}
---
+++ SemaphoreApp/Reader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreApp
+{
+    class Reader
+    {
+        static Semaphore semaphore = new Semaphore(3, 3);
+
+        Thread thread;
+        int count = 3;
+
+        public Reader(int i)
+        {
+            thread = new Thread(Read);
+            thread.Name = $"Читатель {i}";
+            thread.Start();
+        }
+
+        public void Read()
+        {
+            while (count > 0)
+            {
+                semaphore.WaitOne();
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} заходит в библиотеку!");
+                Console.WriteLine($"{Thread.CurrentThread.Name} читает!");
+
+                Thread.Sleep(500);
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} выходит из библиотеки!");
+
+                semaphore.Release();
+                
+                count--;
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
