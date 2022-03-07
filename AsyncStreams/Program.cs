0f583cdfb4e3d7caf2d5ddb71d2ab63bb2642eed
GreedNeSS <ruslan199126@gmail.com>
2022-03-07 20:59:59 +0300

    Added  async streams example

--- AsyncStreams/Program.cs
+++ AsyncStreams/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace AsyncStreams
@@ -7,33 +8,12 @@ namespace AsyncStreams
     {
         static async Task Main(string[] args)
         {
-            var task1 = PrintAsync("H");
-            var task2 = PrintAsync("Hi");
-            var allTasks = Task.WhenAll(task1, task2);
-            try
-            {
-                await allTasks;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception: {ex.Message}");
-                Console.WriteLine($"IsFaulted: {allTasks.IsFaulted}");
-                if (allTasks.Exception is not null)
-                {
-                    foreach (var exception in allTasks.Exception.InnerExceptions)
-                    {
-                        Console.WriteLine($"InnerException: {exception.Message}");
-                    }
-                }
-            }
+            Repository repository = new();
+            IAsyncEnumerable<string> names = repository.GetDataAsync();
 
-            async Task PrintAsync(string message)
+            await foreach (string name in names)
             {
-                // если длина строки меньше 3 символов, генерируем исключение
-                if (message.Length < 3)
-                    throw new ArgumentException($"Invalid string: {message}");
-                await Task.Delay(1000);     // имитация продолжительной операции
-                Console.WriteLine(message);
+                Console.WriteLine(name);
             }
         }
     }
---
+++ AsyncStreams/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{
+    class Repository
+    {
+        string[] data = { "Tom", "Sem", "Kate", "Ruslan", "Marcus" };
+        public async IAsyncEnumerable<string> GetDataAsync()
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine($"Получаем {i + 1} элемент");
+                await Task.Delay(500);
+                yield return data[i];
+            }
+        }
+    }
+}
